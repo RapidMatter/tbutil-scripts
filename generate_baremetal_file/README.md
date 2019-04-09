d2f0f0bc326ca8ca29359556fe959cef3306dca8
Ryan Geyer <ryan.geyer@turbonomic.com>
2019-04-09 10:44:20 -0700

    Add generate_baremetal_file

---
+++ generate_baremetal_file/README.md
@@ -0,0 +1,129 @@
+# Generate BareMetal File
+This tbutil script will extract VMs from a realtime, or topology instance of Turbonomic OpsMgr, and present it in the form of a ["BareMetal" target file](https://turbonomic.com/wp-content/uploads/2019/03/TargetConfiguration_6.3.1.pdf#page=17&zoom=100,0,526).
+
+This can be useful to summarize the VMs basic attributes, and can be used to perform a low fidelity cloud migration plan when it is not possible to connect cloud targets to the realtime OpsMgr.
+
+Note that at the time of this writing (April 2019, circa Turbonomic 6.3.x), there is a conflict between the baremetal documentation linked above, and the actual implementation. Namely the utilization percentage is shown as an ordinal number, rather than a decimal percentage. I.E. 10% == 10.0, not 10% == 0.1 as the documentation states.
+
+## Usage
+There are a few options available when running the BareMetal file generator.
+
+Here is the CLI usage output;
+```
+$ tbutil -s generate_baremetal_file.js -h
+Usage is ..
+
+  tbscript [{cred}] generate_baremetal_file.js [-h]
+    [-d {number_of_days} (default: 30)]
+    [-s {scope} (A unique ID of a group or market. default: Market)]
+    [-e {environment} (default: HYBRID)]
+    [-l {limit} (default: unset, resulting in all available VMs matching the environment and scope)]
+    [-p A flag, when set the resulting file will be based on peak utilization values for each VM. (default: true)]
+    [-a A flag, when set the resulting file will be based on average utilization values for each VM. (default: false)]
+```
+
+### Default settings
+If you do not specify any parameters, this script will;
+* Find all VMs in all environments (HYBRID) for the default market/group (Market)
+* Fetch statistics for the last 30 days
+* Provide utilization values based on the peak-of-peaks
+
+This is equivalent to running the script thusly;
+`$ tbutil -s generate_baremetal_file.js -d 30 -s Market -e HYBRID -p`
+
+### Utilization
+You can choose to populate the "utilization" properties (see [Output](#output) below) with either the "average" utilization, or the peak-of-peaks utilization.
+
+This example is equivalent to the default (not providing either the `-p` or `a` flags) behavior, and will populate the utilization values with the peak-of-peaks value.
+```
+$ tbutil -s generate_baremetal_file.js -p
+```
+
+This example will populate the utilization values with the average of that value, over the [Number of days](#number_of_days).
+```
+$ tbutil -s generate_baremetal_file.js -a
+```
+
+### Number of days
+By default, the script will fetch the last 30 days of statistics for utilization data. This can be changed by supplying a different value for the `-d` parameter. You can choose to fetch more days (60, 90, ???), or fewer.
+
+For an offline topology, or where you only want the "in memory" utilization, provide 0 for the number of days.
+
+### Scope
+You can scope your request to a single group by fetching it's UUID.
+
+To find the UUID of a group you want to scope to, you can use the native tbutil list functionality.
+
+In this example we use tbutil to find the UUID of the group named VMs_vCenter, then use that UUID to scope our BareMetal file generation.
+```
+$ tbutil list groups -l | grep VMs_vCenter
+abc123                Group           VMs_vCenter                                                   ONPREM  Major     false    VirtualMachine       AND
+$ tbutil -s generate_baremetal_file.js -s abc123
+```
+
+### Environment
+You can use the standard Turbonomic "Environment" types to limit your results as well. The options are;
+
+* ONPREM
+* CLOUD
+* HYBRID
+
+By default, `HYBRID` or "everything" is used.
+
+In this example we restrict the search to only on-prem VMs, irrespective of target type (vCenter, HyperV, OpenStack, etc).
+```
+$ tbutil -s generate_baremetal_file.js -e onprem
+```
+
+### Limiting results
+If you're spot checking, or only want a sample, you can limit the number of results that are returned. By default *all* VMs in scope are returned.
+
+This example restricts the output to only the first five VMs it finds.
+```
+$ tbutil -s generate_baremetal_file.js -l 5
+```
+
+## Output
+The script will fetch all VMs for the given parameters, those VMs may be in many different states (active, idle, suspended, etc). A separate output file will be created for each VM state found in the format `hosts-<vm state>.json`.
+
+This is because the actual output does not indicate VM state.
+
+Here is an example output with two VMs.
+```
+{
+  "hosts": [
+    {
+      "cpuSpeedMhz": "2600",
+      "cpuUtilization": "54.79953739398612",
+      "diskDataKB": "138.5",
+      "diskSizeGB": "18.3876953125",
+      "displayName": "VM1",
+      "entityId": "1554827044831-421f4028-1f0e-4432-bd2e-d7ec19217431",
+      "ipAddresses": [
+        "0.0.0.0"
+      ],
+      "memSizeGB": "16",
+      "memUtilization": "7.91260286459039e-11",
+      "netTrafficKB": "0",
+      "numOfCPU": "4",
+      "osName": "CentOS 4/5 or later (64-bit)"
+    },
+    {
+      "cpuSpeedMhz": "1200",
+      "cpuUtilization": "0.25",
+      "diskDataKB": "0",
+      "diskSizeGB": "0",
+      "displayName": "VM2",
+      "entityId": "1554827045482-aws::ca-central-1::VM::i-02221dd8057fcb1ac",
+      "ipAddresses": [
+        "0.0.0.0"
+      ],
+      "memSizeGB": "8",
+      "memUtilization": "0",
+      "netTrafficKB": "1.48",
+      "numOfCPU": "2",
+      "osName": "Linux"
+    }
+  ]
+}
+```
---
+++ generate_baremetal_file/generate_baremetal_file.js
@@ -0,0 +1,274 @@
+/*global args,client,eprintln,interpolate,println, writeJson*/
+/*jslint plusplus: true*/
+
+var Stat = (function () {
+        "use strict";
+
+        function Stat(days) {
+            if (days === null) {
+                this.days = [];
+            } else {
+                this.days = days;
+            }
+        }
+
+        Stat.prototype.days = [];
+
+        Stat.prototype.peak_capacity = function () {
+            var peakval = 0,
+                i = 0;
+
+            if (this.hasOwnProperty("days")) {
+                for (i = 0; i < this.days.length; i++) {
+                    if (this.days[i].hasOwnProperty("capacity")) {
+                        if (this.days[i].capacity.total > peakval) {
+                            peakval = this.days[i].capacity.total;
+                        }
+                    }
+                }
+            }
+            return peakval;
+        };
+
+        Stat.prototype.peak_utilization = function () {
+            var peakval = 0,
+                i = 0;
+
+            if (this.hasOwnProperty("days")) {
+                for (i = 0; i < this.days.length; i++) {
+                    if (this.days[i].hasOwnProperty("values")) {
+                        if (this.days[i].values.max > peakval) {
+                            peakval = this.days[i].values.max;
+                        }
+                    }
+                }
+            }
+            return peakval;
+        };
+
+        Stat.prototype.average_utilization = function () {
+            var totalval = 0,
+                i = 0;
+
+            if (this.hasOwnProperty("days")) {
+                for (i = 0; i < this.days.length; i++) {
+                    totalval = totalval + this.days[i].values.total;
+                }
+            }
+
+            return totalval / this.days.length;
+        };
+
+        Stat.prototype.add_day = function (day) {
+            this.days.push(day);
+        };
+
+        return Stat;
+    }());
+
+var environment_type = "HYBRID",
+    scope = "Market",
+    entity_types = ["VirtualMachine"],
+    peak = true,
+    average = false,
+    limit = 0,
+    number_of_days = 30,
+    vms = [],
+    hosts = {},
+    i,
+    normalized_vm,
+    aspects,
+    aspect_types,
+    aspect_keys,
+    atype,
+    a,
+    stats,
+    stat,
+    stat_name,
+    stat_name_key,
+    filter,
+    day,
+    opts,
+    days_to_ms,
+    stats_req,
+    rtn,
+    cpuStat,
+    memStat,
+    ioStat,
+    netStat,
+    storStat,
+    numVCPUs,
+    state;
+
+function print_usage() {
+    "use strict";
+    eprintln("");
+    eprintln("Usage is ..");
+    eprintln("");
+    eprintln("  tbscript [{cred}] generate_baremetal_file.js [-h]");
+    eprintln("    [-d {number_of_days} (default: 30)]");
+    eprintln("    [-s {scope} (A unique ID of a group or market. default: Market)]");
+    eprintln("    [-e {environment} (default: HYBRID)]");
+    eprintln("    [-l {limit} (default: unset, resulting in all available VMs matching the environment and scope)]");
+    eprintln("    [-p A flag, when set the resulting file will be based on peak utilization values for each VM. (default: true)]");
+    eprintln("    [-a A flag, when set the resulting file will be based on average utilization values for each VM. (default: false)]");
+    eprintln("");
+}
+
+for (i = 0; i < args.length; i++) {
+    if (args[i] === "-h") {
+        print_usage();
+        return 2;
+    }
+    if (args[i] === "-a") {
+        peak = false;
+        average = true;
+    }
+    if (args[i] === "-s") {
+        scope = args[i + 1];
+    }
+    if (args[i] === "-e") {
+        environment_type = args[i + 1];
+    }
+    if (args[i] === "-l") {
+        limit = parseInt(args[i + 1], 10);
+    }
+    if (args[i] === "-d") {
+        number_of_days = parseInt(args[i + 1], 10);
+    }
+    if (args[i] === "-t") {
+        entity_types = args[i + 1].split(",").map(function (s) { "use strict"; return s.toLowerCase(); });
+    }
+}
+
+println(interpolate("Generating a baremetal file for the last ${number_of_days} days of utilization from the environment ${environment_type} scoped to ${scope}"));
+
+opts = {
+    q: "",
+    types: entity_types,
+    environment_type: environment_type,
+    scopes: [scope]
+};
+
+if (limit) {
+    opts.limit = limit;
+} else {
+    limit = "∞";
+}
+
+rtn = client.getSearchResults(opts);
+
+println(interpolate("Found ${rtn.length}/${limit} VMs, iterating over them now."));
+
+days_to_ms = number_of_days *
+        24 * // To Hours
+        60 * // To Minutes
+        60 * // To Seconds
+        1000; // To milliseconds
+stats_req = {
+    "startDate": Date.now() - days_to_ms,
+    "endDate": Date.now()
+};
+
+// For every VM returned by the search..
+for (i = 0; i < rtn.length; i++) {
+    normalized_vm = rtn[i];
+    normalized_vm.aspects = {};
+    normalized_vm.stats = {};
+    normalized_vm.new_stats = {};
+    println(normalized_vm.uuid);
+    if (!hosts.hasOwnProperty(normalized_vm.state)) {
+        hosts[normalized_vm.state] = [];
+    }
+    aspects = client.getAspectsByEntityUuid(rtn[i].uuid);
+    aspect_types = Object.keys(aspects);
+    for (atype = 0; atype < aspect_types.length; atype++) {
+        aspect_keys = Object.keys(aspects[aspect_types[atype]]);
+        for (a = 0; a < aspect_keys.length; a++) {
+            normalized_vm.aspects[aspect_keys[a]] = aspects[aspect_types[atype]][aspect_keys[a]];
+        }
+    }
+
+    // Grab all of the statistics for the last 30 days
+    if (number_of_days > 0) {
+        stats = client.getStatsByUuidQuery(rtn[i].uuid, stats_req);
+    } else {
+        stats = client.getStatsByEntityUuid(rtn[i].uuid, {});
+    }
+    // For each day of stats..
+    for (day = 0; day < stats.length; day++) {
+        // and each stat from that day..
+        for (stat = 0; stat < stats[day].statistics.length; stat++) {
+            // Build a unique "stat name"
+            stat_name_key = "";
+            if (stats[day].statistics[stat].hasOwnProperty("filters")) {
+                for (filter = 0; filter < stats[day].statistics[stat].filters.length; filter++) {
+                    if (stats[day].statistics[stat].filters[filter].type === "key") {
+                        stat_name_key = "_" + stats[day].statistics[stat].filters[filter].value.toLowerCase();
+                    }
+                }
+            }
+            stat_name = stats[day].statistics[stat].name + stat_name_key + "_" + stats[day].statistics[stat].units;
+            stat_name = stat_name.toLowerCase();
+            if (!normalized_vm.new_stats.hasOwnProperty(stat_name)) {
+                normalized_vm.new_stats[stat_name] = [];
+            }
+            normalized_vm.new_stats[stat_name].push(stats[day].statistics[stat]);
+        }
+    }
+
+
+    if (stats.length > 0) {
+        cpuStat = new Stat(normalized_vm.new_stats.vcpu_mhz);
+        memStat = new Stat(normalized_vm.new_stats.vmem_kb);
+        ioStat = new Stat(normalized_vm.new_stats['iothroughput_kbit/sec']);
+        netStat = new Stat(normalized_vm.new_stats['netthroughput_kbit/sec']);
+        storStat = new Stat(normalized_vm.new_stats.storageamount_mb);
+        numVCPUs = 1;
+        if (normalized_vm.hasOwnProperty("aspects") && normalized_vm.aspects.hasOwnProperty("numVCPUs")) {
+            numVCPUs = normalized_vm.aspects.numVCPUs;
+        }
+
+        if (peak === true) {
+            hosts[normalized_vm.state].push({
+                "ipAddresses": ["0.0.0.0"],
+                "displayName": normalized_vm.displayName,
+                "entityId": interpolate("${Date.now()}-${normalized_vm.uuid}"),
+                "osName": normalized_vm.aspects.os,
+                "numOfCPU": numVCPUs.toString(),
+                "memSizeGB": (memStat.peak_capacity() / 1048576).toString(),
+                "memUtilization": ((memStat.peak_utilization() / 1048576) / (memStat.peak_capacity()) / 1048576 * 100).toString(),
+                "cpuSpeedMhz": (cpuStat.peak_capacity() / numVCPUs).toString(),
+                "cpuUtilization": ((cpuStat.peak_utilization() / numVCPUs) / (cpuStat.peak_capacity() / numVCPUs) * 100).toString(),
+                "diskSizeGB": (storStat.peak_utilization() / 1024).toString(),
+                "diskDataKB": ioStat.peak_utilization().toString(),
+                "netTrafficKB": netStat.peak_utilization().toString()
+            });
+        }
+
+        if (average === true) {
+            hosts[normalized_vm.state].push({
+                "ipAddresses": ["0.0.0.0"],
+                "displayName": normalized_vm.displayName,
+                "entityId": interpolate("${Date.now()}-${normalized_vm.uuid}"),
+                "osName": normalized_vm.aspects.os,
+                "numOfCPU": numVCPUs.toString(),
+                "memSizeGB": (memStat.peak_capacity() / 1048576).toString(),
+                "memUtilization": ((memStat.average_utilization() / 1048576) / (memStat.peak_capacity()) / 1048576 * 100).toString(),
+                "cpuSpeedMhz": (cpuStat.peak_capacity() / numVCPUs).toString(),
+                "cpuUtilization": ((cpuStat.average_utilization() / numVCPUs) / (cpuStat.peak_capacity() / numVCPUs) * 100).toString(),
+                "diskSizeGB": (storStat.average_utilization() / 1024).toString(),
+                "diskDataKB": ioStat.average_utilization().toString(),
+                "netTrafficKB": netStat.average_utilization().toString()
+            });
+        }
+    } else {
+        eprintln(interpolate("No stats for ${normalized_vm.uuid}"));
+    }
+}
+
+for (state in hosts) {
+    if (hosts.hasOwnProperty(state)) {
+        writeJson(interpolate("hosts-${state.toLowerCase()}.json"), {"hosts": hosts[state]});
+    }
+}
